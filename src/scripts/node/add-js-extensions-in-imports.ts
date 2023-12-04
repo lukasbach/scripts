97f91eb2cc3507403a1de80772efc3fa10d89367
Lukas Bach <contact@lukasbach.com>
2023-12-04 23:33:10 +0100

    fix js extensions script

--- src/scripts/node/add-js-extensions-in-imports.ts
+++ src/scripts/node/add-js-extensions-in-imports.ts
@@ -1,23 +1,33 @@
-import { glob } from "glob";
+/**
+ * Replaces all imports in the nodejs package to include .js file extensions. Ignores imports of packages (i.e.
+ * non-relative imports), and imports of non-code files. This is part of the the adoption process for ESM builds.
+ */
 
-const files = await glob(path.join(await utils.node.getPackageRoot(), "src/**/*.{ts,tsx,js}"));
+const matcher = path.join(await utils.node.getPackageRoot(), "src/**/*.{ts,tsx,js}").replace(/\\/g, "/");
+const files = await glob(matcher);
 
 for (const file of files) {
   const content = await fs.readFile(file, "utf-8");
   let count = 0;
   const newContent = content.replace(/from\s+['"]([^'"]+)['"]/g, (_, importPath) => {
-    if (!importPath.startsWith(".")) {
+    const ext = path.extname(importPath);
+
+    if (!importPath.startsWith(".") || (ext && ext !== ".ts" && ext !== ".tsx")) {
       return `from "${importPath}"`;
     }
 
     count++;
 
-    if (importPath.endsWith(".ts") || importPath.endsWith(".tsx")) {
-      return `from "${importPath.slice(0, -3)}.js"`;
+    if (ext === ".ts" || ext === ".tsx") {
+      return `from "${importPath.slice(0, -ext.length)}.js"`;
     }
 
     return `from "${importPath}.js"`;
   });
   await fs.writeFile(file, newContent, "utf-8");
-  log.info(`Updated ${file} (${count} replacements)`);
+  if (count > 0) {
+    log.info(`Updated ${file} (${count} replacements)`);
+  } else {
+    log.verbose(`No changes in file ${file}`);
+  }
 }
