46fa98db3ae8df575d4656c7c4cd11229580c9e0
Lukas Bach <contact@lukasbach.com>
2023-12-08 22:29:16 +0100

    change option

--- src/scripts/normalize-json.ts
+++ src/scripts/normalize-json.ts
@@ -1,4 +1,4 @@
 /** Runs JSON.stringify(JSON.parse()) on a file to normalize it. Normalizes with two spaces. */
 
-const file = await ask.text("file", "Which file do you want to normalize?", "file.json");
+const file = await ask.text("f,file", "Which file do you want to normalize?", "file.json");
 await fs.writeJSON(file, await fs.readJSON(file), { spaces: 2 });
